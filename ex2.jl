2c64a929790f748d9abf3d1d7e7ebb4ac54fa6b2
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2021-01-29 08:37:22 -0300

    Update ex2.jl

--- ex2.jl
+++ ex2.jl
@@ -1,18 +1,19 @@
+using JuMP, Ipopt, Plots
 model = Model(Ipopt.Optimizer)
 
 a = 1.0
 b = 3.0
 L = 3.0
-n = 51 #número de divisões de intervalo
+n = 51 #Divisões do intervalo
 h = L/(n-1)
 
 @variable(model, x[1:n])
 @variable(model, u[1:n])
 
 for i = 2:n
-    @NLobjective(model, Min, h*((x[i]*sqrt(1 + u[i]^2) + x[i-1]*sqrt(1 + u[i-1]^2)/2)))
-    @NLconstraint(model, 0.5*h*(sqrt(1 + u[i]) + sqrt(1 + u[i-1])) == L)
-    @constraint(model, x[i] - x[i-1] == h*(u[i-1] + u[i])/2)
+    @NLobjective(model, Min, (h/2) * (x[i]*sqrt(1 + u[i]^2) + x[i-1]*sqrt(1 + u[i-1]^2))
+    @NLconstraint(model, (h/2) * (sqrt(1 + u[i]^2) + sqrt(1 + u[i-1]^2)) == L)
+    @constraint(model, x[i] - x[i-1] == h/2 * (u[i-1] + u[i])
 end
 
 @constraint(model, x[1] == a)
